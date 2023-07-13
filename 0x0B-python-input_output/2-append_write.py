2c8d4c5d005b9df06cf1373014213247de35844a
gasheibrahim <ibrahimgashema154@gmail.com>
2023-07-13 07:39:11 -0700

    changes

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""function that appends a string at the end of a text file"""
-
-
-def append_write(filename="", text=""):
-    """appends a str at the end of a text file and returns the num of chars"""
-    with open(filename, mode="a", encoding="utf-8") as f:
-        return f.write(text)
